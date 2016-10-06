a1067cf46e32df72f6fe3c39b17b7b5d5d0c23b7
Wendel Scardua <wendelscardua@gmail.com>
2016-10-06 18:50:43 -0300

    Add multi-term

--- customizations/misc.el
+++ customizations/misc.el
@@ -24,3 +24,23 @@
 (global-set-key (kbd "C-c g b") 'magit-blame)
 (global-set-key (kbd "C-c g c") 'magit-checkout)
 (global-set-key (kbd "C-c g r b") 'magit-rebase)
+
+;; multi-term
+(defun last-term-buffer (l)
+  "Return most recently used term buffer."
+  (when l
+    (if (eq 'term-mode (with-current-buffer (car l) major-mode))
+        (car l) (last-term-buffer (cdr l)))))
+
+(defun get-term ()
+  "Switch to the term buffer last used, or create a new one if
+    none exists, or if the current buffer is already a term."
+  (interactive)
+  (let ((b (last-term-buffer (buffer-list))))
+    (if (or (not b) (eq 'term-mode major-mode))
+        (multi-term)
+      (switch-to-buffer b))))
+
+(global-set-key (kbd "C-c t") 'get-term)
+(global-set-key (kbd "C-c <left>") 'multi-term-prev)
+(global-set-key (kbd "C-c <right>") 'multi-term-next)
