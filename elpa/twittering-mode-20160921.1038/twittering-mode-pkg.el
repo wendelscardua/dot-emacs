f3089ddd9968cb6d3ba063d95e94d5d5b3399f34
Wendel Scardua <wendelscardua@gmail.com>
2016-10-18 15:27:19 -0200

    Add twittering-mode

---
+++ elpa/twittering-mode-20160921.1038/twittering-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "twittering-mode" "20160921.1038" "Major mode for Twitter" 'nil :stars '(#("358" 0 3 (font-lock-face paradox-star-face))) :url "http://twmode.sf.net/" :keywords '("twitter" "web"))
