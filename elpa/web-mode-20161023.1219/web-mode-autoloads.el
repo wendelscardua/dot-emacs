934360aa053d721dd94ab5e6a1996bc06e092656
Wendel Scardua <wendelscardua@gmail.com>
2016-10-27 16:04:25 -0200

    Update packages
    Package updates

---
+++ elpa/web-mode-20161023.1219/web-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; web-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "web-mode" "web-mode.el" (22546 16703 999079
+;;;;;;  9000))
+;;; Generated autoloads from web-mode.el
+
+(autoload 'web-mode "web-mode" "\
+Major mode for editing web templates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("web-mode-pkg.el") (22546 16704 23540
+;;;;;;  473000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; web-mode-autoloads.el ends here
