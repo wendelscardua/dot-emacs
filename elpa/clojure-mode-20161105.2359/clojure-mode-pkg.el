f238bffe8d7a96d7496ef01bbd084d2d8f753ff8
Wendel Scardua <wendelscardua@gmail.com>
2016-11-07 19:41:53 -0200

    Package updates

---
+++ elpa/clojure-mode-20161105.2359/clojure-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode" "20161105.2359" "Major mode for Clojure code" '((emacs "24.4")) :stars '(#("534" 0 3 (font-lock-face paradox-star-face))) :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
