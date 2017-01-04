fedd676565e8a9f0384e132f38976bbbe16a7c27
Wendel Scardua <wendelscardua@gmail.com>
2017-01-04 12:51:04 -0200

    Update packages
    Update packages

--- elpa/pcache-20160724.1929/pcache-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pcache" "20160724.1929" "persistent caching for Emacs." '((eieio "1.3")) :stars '(#("37" 0 2 (font-lock-face paradox-star-face))))
--- elpa/projectile-20161229.44/projectile-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "projectile" "20161229.44" "Manage and navigate projects in Emacs easily" '((pkg-info "0.4")) :stars '(#("1428" 0 4 (font-lock-face paradox-star-face))) :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
