e2f559b5fca2a91eedcd9ef08948c429c1112d52
Wendel Scardua <wendelscardua@gmail.com>
2017-09-15 21:34:51 -0300

    Update packages

--- elpa/ag-20170712.1549/ag-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ag" "20170712.1549" "A front-end for ag ('the silver searcher'), the C ack replacement." '((dash "2.8.0") (s "1.9.0") (cl-lib "0.5")) :stars '(#("289" 0 3 (font-lock-face paradox-star-face))) :commit "2efb3ea5c3f3b4de79a6f21fa8dbcf90afb758f5")
---
+++ elpa/ag-20170915.1249/ag-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ag" "20170915.1249" "A front-end for ag ('the silver searcher'), the C ack replacement." '((dash "2.8.0") (s "1.9.0") (cl-lib "0.5")) :stars '(#("289" 0 3 (font-lock-face paradox-star-face))) :commit "8501376c112277b98f72963e44bc43e1846d4bcf")
