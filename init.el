0684796a91b47e40cedfafd70f3ba3623efdca01
Wendel Scardua <wendelscardua@gmail.com>
2017-11-01 17:46:50 -0200

    Remove projectile workaround (projectile was fixed)

--- init.el
+++ init.el
@@ -25,11 +25,6 @@
 (add-to-list 'load-path "~/.emacs.d/custom-packages")
 (add-to-list 'load-path "~/.emacs.d/customizations")
 
-;; Temporary workaround for powerline slow project detection
-(setq projectile-mode-line
-      '(:eval (format " Projectile[%s(%s)]"
-                      (projectile-project-name))))
-
 ;; Loads custom packages
 (load "dedicate-windows-manually.el")
 
