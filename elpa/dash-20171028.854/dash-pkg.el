682b09692b5bee78e06ce0501ccc788d07b78a88
Wendel Scardua <wendelscardua@gmail.com>
2017-11-01 17:46:43 -0200

    Update packages

---
+++ elpa/dash-20171028.854/dash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dash" "20171028.854" "A modern list library for Emacs" 'nil :stars '(#("756" 0 3 (font-lock-face paradox-star-face))) :commit "91d8cb01e62bab0d6267d3d4dbcabd6da6fdea78" :keywords '("lists"))
