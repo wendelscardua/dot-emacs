431dcc4ac765345dfc1f644108b1a9154d96c12e
Wendel Scardua <wendelscardua@gmail.com>
2017-11-21 15:15:55 -0200

    Update packages

---
+++ elpa/markdown-mode-20171116.756/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20171116.756" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :stars '(#("235" 0 3 (font-lock-face paradox-star-face))) :commit "7ecf84b281fbf2ae851edb31efef1402c6199339" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
