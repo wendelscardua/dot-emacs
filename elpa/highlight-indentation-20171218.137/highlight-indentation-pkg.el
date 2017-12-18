5f90d0da0a23f11e57896764f0d698e92018a755
Wendel Scardua <wendelscardua@gmail.com>
2017-12-18 13:39:28 -0200

    Update packages

---
+++ elpa/highlight-indentation-20171218.137/highlight-indentation-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-indentation" "20171218.137" "Minor modes for highlighting indentation" 'nil :stars '(#("276" 0 3 (font-lock-face paradox-star-face))) :commit "35e2c1d4f8f368685893128f77f90454cb9c2708" :url "https://github.com/antonj/Highlight-Indentation-for-Emacs")
