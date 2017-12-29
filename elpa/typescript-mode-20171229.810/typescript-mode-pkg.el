f0592c79ba122db227f73347f34936140f60c305
Wendel Scardua <wendelscardua@gmail.com>
2017-12-29 15:41:27 -0200

    Update packages

---
+++ elpa/typescript-mode-20171229.810/typescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typescript-mode" "20171229.810" "Major mode for editing typescript" 'nil :stars '(#("33" 0 2 (font-lock-face paradox-star-face))) :commit "330c887241cf585aa4e2e3f3e9d851f28b92b08e" :url "http://github.com/ananthakumaran/typescript.el" :keywords '("typescript" "languages"))
