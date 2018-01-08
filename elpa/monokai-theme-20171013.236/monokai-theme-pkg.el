09ec5f7b5eca496dd7cc96fc9b5a520f79789d0f
Wendel Scardua <wendelscardua@gmail.com>
2018-01-08 11:26:29 -0200

    Update packages

--- elpa/monokai-theme-20171013.236/monokai-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "monokai-theme" "20171013.236" "A fruity color theme for Emacs." 'nil :stars '(#("221" 0 3 (font-lock-face paradox-star-face))) :commit "019e07947426f0192fc3458f3f72c19031b607de" :url "http://github.com/oneKelvinSmith/monokai-emacs")
--- elpa/twittering-mode-20171231.24/twittering-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "twittering-mode" "20171231.24" "Major mode for Twitter" 'nil :stars '(#("406" 0 3 (font-lock-face paradox-star-face))) :commit "bc0dfd600a19860f79eec1b0f8ab18c8094f79ee" :url "http://twmode.sf.net/" :keywords '("twitter" "web"))
