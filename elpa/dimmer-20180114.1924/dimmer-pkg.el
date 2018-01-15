4b7926873f5e32f0552d855414adbd3b3fb6cc15
Wendel Scardua <wendelscardua@gmail.com>
2018-01-15 15:49:36 -0200

    Update packages

---
+++ elpa/dimmer-20180114.1924/dimmer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dimmer" "20180114.1924" "visually highlight the selected buffer" '((emacs "25")) :stars '("") :commit "0c92663e476413dafd6eb231e19c351bcc195cae" :url "https://github.com/gonewest818/dimmer.el" :keywords '("faces" "editing"))
