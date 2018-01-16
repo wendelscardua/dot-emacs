ad4577aab7f705340c5c21314d173919ccf101be
Wendel Scardua <wendelscardua@gmail.com>
2018-01-16 15:18:26 -0200

    Configure ido-decorations

--- customizations/ui.el
+++ customizations/ui.el
@@ -150,6 +150,16 @@
 
 (setq split-window-preferred-function #'split-window-width-or-height)
 
+
+;; better ido decorations
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "<down>") 'ido-next-match)
+  (define-key ido-completion-map (kbd "<up>") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 ;; dimmer
 (dimmer-mode)
 
