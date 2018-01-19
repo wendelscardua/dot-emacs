4b5235fc727da11b6747fd156743f70893c8a09b
Wendel Scardua <wendelscardua@gmail.com>
2018-01-19 13:51:55 -0200

    Switch paredit for smartparens when editing elisp

--- customizations/elisp-editing.el
+++ customizations/elisp-editing.el
@@ -1,12 +1,10 @@
-;; Automatically load paredit when editing a lisp file
-;; More at http://www.emacswiki.org/emacs/ParEdit
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
+
+(add-hook 'eval-expression-minibuffer-setup-hook #'smartparens-mode)
+(add-hook 'ielm-mode-hook             #'smartparens-mode)
+(add-hook 'lisp-mode-hook             #'smartparens-mode)
+(add-hook 'lisp-interaction-mode-hook #'smartparens-mode)
+(add-hook 'scheme-mode-hook           #'smartparens-mode)
 
 ;; eldoc-mode shows documentation in the minibuffer when writing code
 ;; http://www.emacswiki.org/emacs/ElDoc
