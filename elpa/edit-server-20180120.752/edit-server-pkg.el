fb0d89088a45d49dfb58a0fdfd7fe961925804d8
Wendel Scardua <wendelscardua@gmail.com>
2018-01-23 11:43:40 -0200

    Update packages

---
+++ elpa/edit-server-20180120.752/edit-server-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "edit-server" "20180120.752" "server that responds to edit requests from Chrome" 'nil :stars '(#("305" 0 3 (font-lock-face paradox-star-face))) :commit "32688edc0a8f6c2946d0e94e59cdc43607907a23" :url "https://github.com/stsquad/emacs_chrome")
