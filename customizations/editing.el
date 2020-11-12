5312a5213f5fb52a9a1aaf52159d146346ce91ba
Wendel Scardua <wendelscardua@gmail.com>
2020-11-12 17:47:18 -0300

    Tabnine stuff

--- customizations/editing.el
+++ customizations/editing.el
@@ -137,5 +137,13 @@ If point was already at that position, move point to beginning of line."
 (undo-tree-mode 1)
 (global-set-key (kbd "C-M--") 'undo-tree-visualize)
 
+(add-to-list 'company-backends #'company-tabnine)
+
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
+
 (provide 'editing)
 ;;; editing.el ends here
