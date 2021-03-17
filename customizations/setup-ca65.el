55dd797999053041493379e8e1d3cac5b8e63587
Wendel Scardua <wendelscardua@gmail.com>
2021-03-16 22:39:34 -0300

    Configure ca65-mode indent

--- customizations/setup-ca65.el
+++ customizations/setup-ca65.el
@@ -1,3 +1,7 @@
 (require 'ca65-mode)
 (require 'flycheck-ca65)
 (add-hook 'ca65-mode-hook #'flycheck-ca65-setup)
+(defun ca65-config ()
+  "Configures stuff for this mode."
+  (setq-local tab-width 2))
+(add-hook 'ca65-mode-hook #'ca65-config)
